cc4fb6366736ae5165b10921cad2d4f933a011fa
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-11-16 10:31:33 +0100

    add grabbable doors

--- VR-XRD/Assets/Kasper/Lets Make a VR Game/Oculus Hands/Scripts/AnimateHandOnInput.cs
+++ VR-XRD/Assets/Kasper/Lets Make a VR Game/Oculus Hands/Scripts/AnimateHandOnInput.cs
@@ -9,14 +9,18 @@ public class AnimateHandOnInput : MonoBehaviour
     public InputActionProperty pinchAnimationAction;
     public InputActionProperty gripAnimationAction;
     public Animator handAnimator;
+    private static readonly int Trigger = Animator.StringToHash("Trigger");
+    private static readonly int Grip = Animator.StringToHash("Grip");
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Trigger", triggerValue);
+        var triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat(Trigger, triggerValue);
 
-        float gripValue = gripAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Grip", gripValue);
+        var gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat(Grip, gripValue);
     }
 }
+
+
